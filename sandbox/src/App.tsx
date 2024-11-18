64f8aada7743d6a6668f221f6aa81cb6a3a8dbc9
SB <sarahbrown.slb@gmail.com>
2024-11-18 21:35:51 +1030

    refactoring sandbox app to be more of a demo (WIP)

--- sandbox/src/App.tsx
+++ sandbox/src/App.tsx
@@ -13,112 +13,117 @@ import {
   TextWithTooltip,
   TwoColumnBlock,
 } from "../../src";
-import { ThemeProvider } from "../../src/theme/ThemeContext";
-import { techIcons, linkIcons, cardData } from "./content";
-
-const HeroSection = () => (
-  <FullWidthSection>
-    <ImageTextBlock
-      imageSrc="./images/sarah-brown-web-developer.png"
-      altText="hero image"
-    >
-      <Heading level={1}>
-        Hi, I'm Sarah!👋🏼 <br />
-        I'm a{" "}
-        <TextWithTooltip
-          text="cloud native,"
-          tooltipContent="I design applications specifically for the cloud, so they can scale efficiently, recover quickly, and adapt as needed."
-          gradientIndex={1}
-        />{" "}
-        <TextWithTooltip
-          text="full-stack"
-          tooltipContent="I handle both front-end and back-end development, managing everything from the user interface to the server"
-          gradientIndex={2}
-        />{" "}
-        <TextWithTooltip
-          text="web developer"
-          tooltipContent="I create and maintain websites and web applications"
-          gradientIndex={3}
-        />
-        .
-      </Heading>
-    </ImageTextBlock>
-  </FullWidthSection>
-);
-
-const Section = ({
-  title,
-  children,
-}: {
-  title: string;
-  children: React.ReactNode;
-}) => (
-  <FullWidthSection>
-    <Heading level={2}>{title}</Heading>
-    <Spacer size="medium" />
-
-    {children}
-  </FullWidthSection>
-);
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faReact, faNodeJs, faAws } from "@fortawesome/free-brands-svg-icons";
 
 const App = () => (
-  <ThemeProvider>
-    <main>
-      <HeroSection />
+  <main>
+    {/* Hero Section */}
+    <FullWidthSection>
+      <Heading level={1}>Esbie UI</Heading>
+      <Spacer size="medium" />
 
-      {/* <Section title="My Work">
-        <CardGallery
-          cards={cardData.map((card, index) => (
-            <div key={index}>
-              <h2>{card.title}</h2>
-              <p>{card.description}</p>
-            </div>
-          ))}
-          galleryId="projects"
-        />
-      </Section> */}
+      <Paragraph>
+        A library of reusable, customisable components built with React,
+        TypeScript, and SCSS modules.
+      </Paragraph>
+      <Spacer size="large" />
+      {/* <Button onClick={() => alert("You clicked the button!")}>Click Me</Button> */}
+    </FullWidthSection>
 
-      <FullWidthSection
-        backgroundImage="./images/dreamy-pastel-clouds.jpeg"
-        backgroundType="content"
-        alignment="center"
-        glassEffect={true}
-      >
-        {" "}
-        <Spacer size="large" />
-        <Heading level={2}>About Me</Heading>
-        <Spacer size="small" />
-        <Paragraph size="medium">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Illud non
-          continuo, ut aeque incontentae. Oratio me istius philosophi non
-          offendit; Duo Reges: constructio interrete. Nihil opus est exemplis
-          hoc facere longius.
-        </Paragraph>{" "}
-        <Spacer size="large" />
-      </FullWidthSection>
+    {/* Icons Section */}
+    <FullWidthSection>
+      <Heading level={2}>Icons & Icon Gallery</Heading>
+      <Spacer size="large" />
 
-      <Section title="The Tools I Use">
-        <IconGallery icons={techIcons} withCaptions={true} />
-      </Section>
+      <IconGallery
+        icons={[
+          {
+            icon: <i className="fa-brands fa-react"></i>,
+            caption: "React",
+          },
+          {
+            icon: <i className="fa-brands fa-node-js"></i>,
+            caption: "Node.js",
+          },
+          {
+            icon: <i className="fa-brands fa-aws"></i>,
+            caption: "AWS",
+          },
+        ]}
+        withCaptions
+        size="large"
+      />
+    </FullWidthSection>
 
-      <FullWidthSection>
-        <TwoColumnBlock
-          leftColumn={
-            <HeadingWithIcon
-              level={2}
-              icon={<i className="fa-solid fa-arrow-right"></i>}
-              iconPosition="right"
-            >
-              More from me
-            </HeadingWithIcon>
-          }
-          rightColumn={
-            <IconGallery icons={linkIcons} withCaptions={true} size="large" />
-          }
-        />
-      </FullWidthSection>
-    </main>
-  </ThemeProvider>
+    {/* Cards Section */}
+    <FullWidthSection>
+      <Heading level={2}>Card Gallery</Heading>
+      {/* <CardGallery
+        cards={[
+          {
+            title: "Project 1",
+            description: "A brief description of Project 1.",
+            link: "https://example.com/project1",
+            backgroundImage: "https://picsum.photos/300/200?random=1",
+          },
+          {
+            title: "Project 2",
+            description: "A brief description of Project 2.",
+            link: "https://example.com/project2",
+            backgroundImage: "https://picsum.photos/300/200?random=2",
+          },
+        ]}
+      /> */}
+    </FullWidthSection>
+
+    <FullWidthSection>
+      <Heading level={2}>Two Column Block</Heading>
+      <Spacer size="large" />
+
+      <TwoColumnBlock
+        leftColumn={
+          <ImageTextBlock
+            imageSrc="https://picsum.photos/200/300"
+            altText="Column 1 Title"
+          >
+            This is some content for the first column.
+          </ImageTextBlock>
+        }
+        rightColumn={
+          <ImageTextBlock
+            imageSrc="https://picsum.photos/200/300?grayscale"
+            altText="Column 2 Title"
+          >
+            This is some content for the second column.
+          </ImageTextBlock>
+        }
+      />
+    </FullWidthSection>
+
+    {/* Tooltip Section */}
+    <FullWidthSection>
+      <Heading level={2}>Text With Tooltip</Heading>
+      <Spacer size="large" />
+
+      <Paragraph>
+        This is a demo of the{" "}
+        <TextWithTooltip
+          text="tooltip"
+          tooltipContent="Here is some helpful information!"
+        />{" "}
+        component. Hover over the word to see more information.
+      </Paragraph>
+    </FullWidthSection>
+
+    {/* Spacer Demo */}
+    <FullWidthSection>
+      <Heading level={2}>Spacer Demo</Heading>
+      <Paragraph>Here is some text followed by a spacer:</Paragraph>
+      <Spacer size="large" />
+      <Paragraph>Here is more text after the spacer.</Paragraph>
+    </FullWidthSection>
+  </main>
 );
 
 export default App;
